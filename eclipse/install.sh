511425a3ba1b779cf54cc4ef7e6a4ef19292af3a
Aihua <aihua.tech@gmail.com>
2014-06-29 22:43:20 +0800

    Add Shell Profile Script For Ant/Jse/Maven

--- eclipse/install.sh
+++ eclipse/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-dist_version="4.3.1"
+dist_version="4.4"
 install_root="/opt/install/$dist_version"
-#eclipse_root="/opt/eclipse"
-eclipse_root="/mnt/temp/eclipse"
+eclipse_root="/opt/eclipse"
+#eclipse_root="/mnt/temp/eclipse"
 dropins_root="$eclipse_root/eclipse/dropins"
 
-#tar -C $eclipse_root -zxvf $install_root/eclipse-platform-$dist_version-linux-gtk-x86_64.tar.gz
-unzip $install_root/eclipse-platform-4.3.1-win32.zip -d $eclipse_root
+tar -C $eclipse_root -zxvf $install_root/eclipse-platform-$dist_version-linux-gtk-x86_64.tar.gz
+#unzip $install_root/eclipse-platform-4.3.1-win32.zip -d $eclipse_root
 
 function mkdir_with_eclipse () {
 	if [ ! -z "$1" ] ; then 
@@ -36,31 +36,31 @@ function handle_artifact () {
 }
 
 declare -A artifact_files
-artifact_files['jdt']="org.eclipse.jdt-4.3.1.zip"
-artifact_files['pde']="org.eclipse.pde-4.3.1.zip"
-artifact_files['graphti']="org.eclipse.graphiti.site_0.10.1.201309180838.zip"
-artifact_files['cdt']="cdt-master-8.2.1.zip"
-artifact_files['wtp4x']="wtp4x-repo-R-3.5.1-20130913223750.zip"
-artifact_files['wb']="WB_v1.6.1_UpdateSite_for_Eclipse4.3.zip"
+artifact_files['jdt']="org.eclipse.jdt-${dist_version}.zip"
+artifact_files['pde']="org.eclipse.pde-${dist_version}.zip"
+artifact_files['graphti']="org.eclipse.graphiti.site_0.11.0.201405280646.zip"
+artifact_files['cdt']="cdt-8.4.0.zip"
+artifact_files['wtp4x']="wtp4x-repo-R-3.6.0-20140602160322.zip"
+artifact_files['wb']="WB_v1.7.0_UpdateSite_for_Eclipse4.4.zip"
 
-artifact_files['emf']="emf-runtime-2.9.1.zip"
-artifact_files['emf-transaction']="emf-transaction-runtime-1.7.0.zip"
-artifact_files['emf-validation']="emf-validation-runtime-1.7.0.zip"
-artifact_files['xsd']="xsd-runtime-2.9.1.zip"
-artifact_files['gef']="GEF-runtime-3.9.1.zip"
-artifact_files['dtp']="dtp_1.11.1.zip"
-artifact_files['birt']="birt-report-framework-4_3_1.zip"
+artifact_files['emf']="emf-runtime-2.10.0.zip"
+artifact_files['emf-transaction']="emf-transaction-runtime-1.8.0.zip"
+artifact_files['emf-validation']="emf-validation-runtime-1.8.0.zip"
+artifact_files['xsd']="xsd-runtime-2.10.0.zip"
+artifact_files['gef']="GEF-runtime-3.9.100.zip"
+artifact_files['dtp']="dtp-p2repo_1.12.0.zip"
+artifact_files['birt']="birt-report-framework-4_4_0-20140611.zip"
 
 artifact_files['spket']="spket-1.6.23.zip"
 
-artifacts="jdt pde graphti cdt wtp4x wb"
+artifacts="jdt pde dtp graphti cdt wtp4x wb"
 for artifact in $artifacts ; do
 	mkdir_with_eclipse $artifact
 	unzip $install_root/${artifact_files[$artifact]} -d $dropins_root/$artifact/eclipse
 	handle_artifact $artifact
 done
 
-artifacts="emf emf-transaction emf-validation xsd gef dtp birt spket"
+artifacts="emf emf-transaction emf-validation xsd gef birt spket"
 
 for artifact in $artifacts ; do
 	mkdir -p $dropins_root/$artifact
