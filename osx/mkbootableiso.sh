6777e1b98877f704f0a72db764b5f09ce607707c
Aihua <bizstar@163.com>
2017-09-12 13:28:14 +0800

    create iso for macOS Sierra

--- osx/mkbootableiso.sh
+++ osx/mkbootableiso.sh
@@ -1,19 +1,19 @@
 #!/bin/bash  
-# Convert InstallESD.dmg(OS X El Capitan Image) to .iso
-# Put it in /Applications/Install\ OS\ X\ El\ Capitan.app/ directory and run it.
+# Convert InstallESD.dmg(macOS Sierra Image) to .iso
+# Put it in /Applications/Install macOS Sierra.app directoroy and run it.
 
 hdiutil attach ./Contents/SharedSupport/InstallESD.dmg -noverify -nobrowse -mountpoint /Volumes/install_app  
-hdiutil convert /Volumes/install_app/BaseSystem.dmg -format UDSP -o ./Capitan  
-hdiutil resize -size 8g ./Capitan.sparseimage  
-hdiutil attach ./Capitan.sparseimage -noverify -nobrowse -mountpoint /Volumes/install_build  
+hdiutil convert /Volumes/install_app/BaseSystem.dmg -format UDSP -o ./Sierra  
+hdiutil resize -size 8g ./Sierra.sparseimage  
+hdiutil attach ./Sierra.sparseimage -noverify -nobrowse -mountpoint /Volumes/install_build  
 rm -fr /Volumes/install_build/System/Installation/Packages  
 cp -rp /Volumes/install_app/Packages /Volumes/install_build/System/Installation/  
 cp -rp /Volumes/install_app/BaseSystem.chunklist /Volumes/install_build  
 cp -rp /Volumes/install_app/BaseSystem.dmg /Volumes/install_build  
 hdiutil detach /Volumes/install_app  
 hdiutil detach /Volumes/install_build  
-hdiutil resize -size `hdiutil resize -limits ./Capitan.sparseimage | tail -n 1 | awk '{ print $1 }'`b ./Capitan.sparseimage  
-hdiutil convert ./Capitan.sparseimage -format UDTO -o /tmp/Capitan  
-rm -fr ./Capitan.sparseimage  
-#mv ./Capitan.cdr ~/Desktop/Capitan.iso  
-hdiutil makehybrid -iso -joliet -o /tmp/Capitan.iso /tmp/Capitan.cdr
+hdiutil resize -size `hdiutil resize -limits ./Sierra.sparseimage | tail -n 1 | awk '{ print $1 }'`b ./Sierra.sparseimage  
+hdiutil convert ./Sierra.sparseimage -format UDTO -o /tmp/Sierra  
+rm -fr ./Sierra.sparseimage  
+#mv ./Sierra.cdr ~/Desktop/Sierra.iso  
+hdiutil makehybrid -iso -joliet -o /tmp/Sierra.iso /tmp/Sierra.cdr
