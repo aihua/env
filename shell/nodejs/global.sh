4673a809bf4da3a3addda17e2d7eacae62682dde
aihua <bizstar@163.com>
2018-03-08 21:45:20 +0800

    Add global packages for node.js

---
+++ shell/nodejs/global.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+npm install -g bower
+npm install -g browserify
+npm install -g eslint
+npm install -g grunt-cli
+npm install -g gulp-cli
+npm install -g jshint
+npm install -g prettier
+npm install -g typescript
+npm install -g webpack
+npm install -g create-react-app
+npm install -g roadhog 
+npm install -g dva-cli
+npm install -g ant-design-pro-cli
