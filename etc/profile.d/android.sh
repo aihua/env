81de36beabaa09f2761e66afafd12d9b3b3a05f2
Aihua <bizstar@163.com>
2018-05-01 23:01:05 +0800

    Update for Android NDK

--- etc/profile.d/android.sh
+++ etc/profile.d/android.sh
@@ -6,9 +6,24 @@ if [ -z "${ANDROID_HOME}" ] ; then
 
 	if [ -x "${android_sdk_path}/tools/android" ] ; then
 		ANDROID_HOME=${android_sdk_path} ; export ANDROID_HOME
+        ANDROID_SDK_ROOT=${ANDROID_HOME} ; export ANDROID_SDK_ROOT
 		PATH=${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/tools/proguard/bin:${ANDROID_HOME}/platform-tools:${ANDROID_HOME}/build-tools/27.0.3:${PATH} ; export PATH
+        ANDROID_EMULATOR_HOME=/data/repo/android ; export ANDROID_EMULATOR_HOME
 	fi
 	
 	unset android_sdk_path
 	unset android_sdk_version
+fi
+
+if [ -z "${ANDROID_NDK_ROOT}" ] ; then
+	android_ndk_version="r16b"
+	android_ndk_path="/opt/android/ndk/${android_ndk_version}"
+
+	if [ -x "${android_ndk_path}/ndk-which" ] ; then
+		ANDROID_NDK_ROOT=${android_ndk_path} ; export ANDROID_NDK_ROOT
+		PATH=${ANDROID_NDK_ROOT}:${PATH} ; export PATH
+	fi
+	
+	unset android_ndk_path
+	unset android_ndk_version
 fi
