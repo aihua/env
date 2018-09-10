7e9bdd66eb82983e983c30ba7157ad3d287bbcf8
Aihua <bizstar@163.com>
2018-09-10 14:27:19 +0800

    RabbitMQ Config

--- etc/profile.d/erlang.sh
+++ etc/profile.d/erlang.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 if [ -z "${ERLANG_HOME}" ] ; then
-	erlang_version="21.0.7"
+	erlang_version="21.0.8"
 	erlang_path="/opt/erlang/${erlang_version}"
 
 	if [ -x "${erlang_path}/bin/erl" ] ; then
 		ERLANG_HOME=${erlang_path} ; export ERLANG_HOME
 		PATH=${ERLANG_HOME}/bin:${PATH} ; export PATH
+
+        ERL_EPMD_ADDRESS=127.0.0.1 ; export ERL_EPMD_ADDRESS
 	fi
 	
 	unset erlang_path
