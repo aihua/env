b6c16d73a2cf25023e8efdd629357dfcf25a9078
Aihua <bizstar@163.com>
2018-10-09 15:58:40 +0800

    Update version of ant,gradle,maven,node

--- etc/profile.d/gradle.sh
+++ etc/profile.d/gradle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "${GRADLE_HOME}" ] ; then
-	gradle_version="4.7"
+	gradle_version="4.10.2"
 	gradle_path="/opt/gradle/${gradle_version}"
 
 	if [ -x "${gradle_path}/bin/gradle" ] ; then
--- etc/profile.d/maven.sh
+++ etc/profile.d/maven.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "${M2_HOME}" ] ; then
-	maven_version="3.5.3"
+	maven_version="3.5.4"
 	maven_path="/opt/maven/${maven_version}"
 
 	if [ -x "${maven_path}/bin/mvn" ] ; then
