8f4d5b7281d40db6ca106ac0cf6aef3d51d793df
Aihua <bizstar@163.com>
2021-10-16 11:55:04 +0800

    Update brew for mac

--- osx/etc/profile.d/brew.sh
+++ osx/etc/profile.d/brew.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 if [ -x "/usr/local/bin/brew" ] ; then
-   HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles ; export HOMEBREW_BOTTLE_DOMAIN 
+    if [[ "$(uname -s)" == "Linux" ]]; then BREW_TYPE="linuxbrew"; else BREW_TYPE="homebrew"; fi
+
+    export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+    export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/${BREW_TYPE}-core.git"
+    export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/${BREW_TYPE}-bottles"
 fi
