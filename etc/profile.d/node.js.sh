a5088f39f504de6a7529772f7a2695704156f564
Aihua <bizstar@163.com>
2021-10-16 12:06:09 +0800

    Update node.js shell

--- etc/profile.d/node.js.sh
+++ etc/profile.d/node.js.sh
@@ -3,10 +3,11 @@
 if [ -z "${NODE_JS_HOME}" ] ; then
 	node_js_version="14.18.1"
 	node_js_path="/opt/node.js/${node_js_version}"
+    node_prefix_path="/data/repo/node.js/global"
 
 	if [ -x "${node_js_path}/bin/node" ] ; then
 		NODE_JS_HOME=${node_js_path} ; export NODE_JS_HOME
-		PATH=${NODE_JS_HOME}/bin:${PATH} ; export PATH
+		PATH=${NODE_JS_HOME}/bin:${node_prefix_path}/bin:${PATH} ; export PATH
 	fi
 	
 	unset node_js_path
