ee6eeaffb0c9c05133ecbb8a405b857c7510314d
Aihua <bizstar@163.com>
2023-07-31 15:20:14 +0800

    Shell for MySQL with Tables Dumped

--- shell/mysql/dump.sh
+++ shell/mysql/dump.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-host=""
+host="127.0.0.1"
+port="3306"
 user=""
 password=""
 database="demo"
+tables=""
 dist="xxx-`date +%y%m%d%H%M%S`.sql"
 
-mysqldump --host="${host}" --user="${user}" --password="${password}" --set-gtid-purged=OFF --add-drop-table --add-drop-trigger --single-transaction -ER ${database} > ${dist}
+mysqldump --host="${host}" --port="${port}"  --user="${user}" --password="${password}" --set-gtid-purged=OFF --add-drop-table --add-drop-trigger --single-transaction -ER ${database} ${tables} > ${dist}
+
+# MySQL 5.x
+# mysqldump --host="${host}" --port="${port}"  --user="${user}" --password="${password}" --column-statistics=0  --set-gtid-purged=OFF --add-drop-table --add-drop-trigger --single-transaction -ER ${database} ${tables} > ${dist}
--- shell/mysql/import.sh
+++ shell/mysql/import.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-host=""
+host="127.0.0.1"
+port="3306"
 user=""
 password=""
 database=""
 src="patch-20180808.sql"
 
-mysql --host="${host}" --user="${user}" --password="${password}" ${database} <  ${src} > out.log 2>&1
+mysql --host="${host}" --port="${port}" --user="${user}" --password="${password}" ${database} <  ${src} > out.log 2>&1
