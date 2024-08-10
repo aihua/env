7b93a5ba98e02d5ceee882d02271b61f84420aec
Aihua <aihua@users.noreply.github.com>
2024-08-10 22:36:23 +0800

    Update for video cuttings

--- shell/video/cut.sh
+++ shell/video/cut.sh
@@ -2,12 +2,13 @@
 
 # cut video from $start to the last $end seconds.
 
-start="18"
-end="8"
+start="8"
+end="0"
 
 for video in *.mp4
 do
-  duration=`ffprobe -v error -show_entries format=duration -of csv=p=0 ${video}`
+  duration=`ffprobe -v error -show_entries format=duration -of csv=p=0 "${video}"`
   duration=`bc -e ${duration}-${end}`
-  ffmpeg -ss ${start} -to ${duration} -i ${video} -c copy ../new/${video}
+  ffmpeg -ss ${start} -to ${duration} -i "${video}" -c copy "../new/${video}"
+  touch -r "${video}" "../new/${video}"
 done
