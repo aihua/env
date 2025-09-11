2170ec687db9a6f746911e00b75d5da31a801576
Aihua <1410274-aihua@users.noreply.gitlab.com>
2025-09-11 12:21:24 +0800

    Rust环境变量

--- etc/profile.d/rust.sh
+++ etc/profile.d/rust.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
 if [ -z "${RUST_HOME}" ] ; then
-	rust_version="1.78.0"
-	rust_path="/opt/rust/${rust_version}"
-    rust_toolchain_path="${rust_path}/toolchains/stable-x86_64-apple-darwin"
+  rust_version="1.89.0"
+  rust_path="/opt/rust/${rust_version}"
 
-	if [ -x "${rust_toolchain_path}/bin/rustc" ] ; then
-		RUST_HOME=${rust_toolchain_path} ; export RUST_HOME
-        CARGO_HOME=/data/repo/cargo ; export CARGO_HOME
-        RUSTUP_HOME=${rust_path} ; export RUSTUP_HOME
-		PATH=${RUST_HOME}/bin:${RUSTUP_HOME}/bin:${PATH} ; export PATH
-	fi
-	
-	unset rust_path
-	unset rust_version
+  if [ -x "${rust_path}/bin/rustc" ] ; then
+    RUST_HOME=${rust_path} ; export RUST_HOME
+    CARGO_HOME=/data/repo/cargo ; export CARGO_HOME
+    PATH=${RUST_HOME}/bin:${CARGO_HOME}/bin:${PATH} ; export PATH
+  fi
+
+  unset rust_path
+  unset rust_version
 fi
